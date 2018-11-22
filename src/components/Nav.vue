e1d7ea9cb2fc2e9e2f9b74e19158fb438a1d1000
salazar273 <msalazar273@gmail.com>
2018-11-22 10:22:14 -0500

    Activities Page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,28 +8,28 @@
       <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/about">About</router-link>
       </li>
-      <li v-if="!userReady()" class="nav-item">
+      <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/join">Signup/Login</router-link>
       </li>
       <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/news">News</router-link>
       </li>
-      <li v-if="userReady()" class="nav-item">
+      <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/friends">Friend Feed</router-link>
       </li>
-      <li v-if="userReady()" class="nav-item">
+      <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/friends">Activity Log</router-link>
       </li>
-      <li v-if="userReady()" class="nav-item">
+      <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/friends">Food Log</router-link>
       </li>
-      <li v-if="userReady()" class="nav-item">
+      <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/friends">Calorie Report</router-link>
       </li>
-      <li v-if="userReady()" class="nav-item">
+      <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/friends">Challenges</router-link>
       </li>
-      <li v-if="userReady()" class="nav-item">
+      <li class="nav-item">
         <router-link class="nav-link" exact-active-class="active" to="/friends">Notifications</router-link>
       </li>
     </ul>
@@ -39,25 +39,5 @@
 </template>
 
 <script>
- import * as api from '@/services/api_access';
- let loopTimer = null;
- export default {
-     data(){
-         return {
-            users: []
 
-          }
-     },
-     created(){
-         loopTimer = setInterval(this.refresh, 1000)
-     },
-     methods: {
-          refresh(){
-             api.GetUsers()
-             .then(x=> this.users = x)
-        },
-         userId: ()=> api.userId,
-         userReady: ()=> api.userReady
-     }
- }
  </script> 
